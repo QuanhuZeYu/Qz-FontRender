d55f71bc82f90a67b3336cfc6714a2310ade549a
泉户泽雨 <1624910218@qq.com>
2025-09-27 20:45:37 +0800

    调整默认参数

--- src/main/java/club/heiqi/qz_fontrender/Config.java
+++ src/main/java/club/heiqi/qz_fontrender/Config.java
@@ -54,7 +54,7 @@ public class Config {
         charSize = config.getFloat("charSize", Configuration.CATEGORY_GENERAL, 9f, -Float.MAX_VALUE, Float.MAX_VALUE, "字体大小");
         lineSpacing = config.getFloat("lineSpacing", Configuration.CATEGORY_GENERAL, 1.0f, -Float.MAX_VALUE, Float.MAX_VALUE, "行间距");
         sigma = config.getFloat("sigma", Configuration.CATEGORY_GENERAL, 2.0f, 0.0f, 64.0f, "高斯核标准差");
-        blurRadius = config.getFloat("blurRadius", Configuration.CATEGORY_GENERAL, 2f, 0.0f, 64.0f, "模糊程度");
+        blurRadius = config.getFloat("blurRadius", Configuration.CATEGORY_GENERAL, 1f, 0.0f, 64.0f, "模糊程度");
         smoothRangeMin = config.getFloat("smoothRangeMin", Configuration.CATEGORY_GENERAL, 0f, 0.0f, 1.0f, "平滑最小参数");
         smoothRangeMax = config.getFloat("smoothRangeMax", Configuration.CATEGORY_GENERAL, 1f, 0.0f, 1.0f, "平滑最大参数");
 
